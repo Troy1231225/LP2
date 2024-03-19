368dc5405816b7059a31a7802cc4e4abb748573e
Troy1231225 <160743757+Troy1231225@users.noreply.github.com>
2024-03-19 20:23:26 +0800

    se2

--- 6b964313b1da318dc863680b2c292cca.static.js
+++
@@ -1,2 +0,0 @@
-var ii=0;if(typeof history.pushState==="function"){history.pushState("back",null,null);window.onpopstate=function(){history.pushState('back',null,null);};}
-setTimeout(function(){ii=1;},200);
